1bd0ace186b36414c2a689196e514f3960ed3d9b
Maha Lakshme <mahalaks@thoughtworks.com>
2016-01-04 16:40:25 +0530

    #002:Maha/Dinesh:1.Incorporated facility to add other skills 2.Changed error messages

--- www/js/create-candidate-profile-controller.js
+++ www/js/create-candidate-profile-controller.js
@@ -3,10 +3,11 @@ angular.module('starter').controller('createCandidateProfileController', functio
     java: false,
     csharp: false,
     ruby: false,
-    python: false
+    python: false,
+    other: false
   };
   $scope.validate = function () {
-    return !($scope.candidate.java || $scope.candidate.csharp || $scope.candidate.ruby || $scope.candidate.python) || $scope.candidateForm.$invalid;
+    return !($scope.candidate.java || $scope.candidate.csharp || $scope.candidate.ruby || $scope.candidate.python || ($scope.candidate.other)) || $scope.candidateForm.$invalid;
   };
   $scope.redirectToScheduleInterviewPage = function () {
     if ($scope.candidateForm.$valid) {
