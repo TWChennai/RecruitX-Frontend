b346bd719c16e6737db31c3d0dbfd35cd361ccb9
Kausalya Mani <kausalyamani@INKAUSALYA.local>
2016-01-20 13:16:45 +0530

    Kausalya: Schedule Interview: Added logic to display date picker and reflect user selection on UI, construct JSON structure for interview schedule as expected by POST action, enable/disable SAVE button

--- www/js/schedule-interview-controller.js
+++ www/js/schedule-interview-controller.js
@@ -1,53 +1,52 @@
 angular.module('starter')
-.controller('scheduleInterviewController', function($scope, $stateParams, $cordovaDatePicker, recruitFactory){
-
-  $stateParams.candidate.rounds = [];
+.controller('scheduleInterviewController', function($scope, $stateParams, $cordovaDatePicker, recruitFactory,  $filter){
+  $stateParams.candidate.interviewSchedule = [];
   var interviewMap = {};
   /* Get Interview Rounds */
   recruitFactory.getInterviewRounds(function(interviewRounds){
     $scope.interviewRounds = interviewRounds;
   });
-  $scope.dateTime = function(args) {
+
+  $scope.dateTime = function(index) {
+
       var options = {
-            date: new Date(),
-            mode: 'datetime',
-            allowOldDates: false
-            };
-    $cordovaDatePicker.show(options).then(function(dateTime) {
-     console.log('datetime' + dateTime);
-     var date = new Date(dateTime);
-     console.log('date:' + date);
-     var hours = date.getHours();
-     var ampm = hours >= 12 ? 'pm' : 'am';
-     hours = hours % 12;
-     hours = hours ? hours : 12;
-     var timing = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + '  ' + hours + ':' + date.getMinutes() + ' ' + ampm;
+        date: new Date(),
+        mode: 'datetime',
+        allowOldDates: false,
+        minDate: new Date()
+      };
 
-     $scope.interviewRounds[args].displayDateTime = timing;
-     $scope.interviewRounds[args].actualDateTime = dateTime;
-     interviewMap[$scope.interviewRounds[args].id] = dateTime;
-     console.log("interviewMap" + interviewMap);
-     for (var i in interviewMap){
-         console.log('Key in datetime() is: ' + i + '. Value is: ' + interviewMap[i]);
-     }
+      $cordovaDatePicker.show(options).then(function (dateTime) {
+        $scope.interviewRounds[index].dateTime = dateTime;
+
+      });
+    };
+
+    $scope.isFormInvalid = function(){
+      var inValid = true;
+      for (var interviewRoundIndex in $scope.interviewRounds) {
+        if ($scope.interviewRounds[interviewRoundIndex].dateTime !== undefined) {
+          return false;
+        }
+      }
+      return true;
+    };
+
+    $scope.postCandidate = function () {
+      for (var interviewRoundIndex in $scope.interviewRounds) {
+        if ($scope.interviewRounds[interviewRoundIndex].dateTime !== undefined) {
+          var formattedDateTime = $filter('date')($scope.interviewRounds[interviewRoundIndex].dateTime, 'yyyy-MM-dd HH:mm:ss');
+          $stateParams.candidate.interviewSchedule.push({'interview_id': $scope.interviewRounds[interviewRoundIndex].id,
+          'candidate_interview_date_time': formattedDateTime});
+        }
+      }
+
+      recruitFactory.saveCandidate($stateParams, function (res) {
+        //alert(res);
+        console.log(res);
+      });
+
+      // var candidate = new Candidate($stateParams)
+      // candidate.$save()
+    };
   });
-  };
-  //console.log($stateParams);
-  //console.log(recruitFactory);
-  $scope.postCandidate = function(){
-    for (var i in interviewMap){
-      $stateParams.candidate.rounds.push({id:i, timei:interviewMap[i]});
-      console.log('Key is: ' + i + '. Value is: ' + interviewMap[i]);
-    }
-
-    //var candidate = new Candidate($stateParams);
-    //candidate.$save();
-    //alert('test');
-    //console.log($stateParams);
-    //console.log(recruitFactory);
-    recruitFactory.saveCandidate($stateParams, function (res) {
-      //alert(res);
-      console.log(res);
-    });
-  }
-});
