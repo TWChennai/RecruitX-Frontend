08c17d05e1c7b543bedbaf198fb7d2aff5e78774
Subha <subhamanoharanbe@gmail.com>
2016-01-25 19:00:02 +0530

    #2:Subha:Adding Pull to refresh for panelist sign-up

--- www/js/app.js
+++ www/js/app.js
@@ -81,5 +81,5 @@ angular.module('starter', ['ionic','ui.router', 'ngCordova', 'ngResource'])
       });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/candidates');
+  $urlRouterProvider.otherwise('/panelist-signup');
 });
--- www/js/panelist-signup-controller.js
+++ www/js/panelist-signup-controller.js
@@ -1,7 +1,25 @@
 angular.module('starter')
-	.controller('panelistSignupController', function() {
-  'use strict';
-	})
-	.controller('interviewDetailsController', function() {
+ .controller('panelistSignupController', function($scope, $http) {
+    'use strict';
 
+    $scope.items = [1,2,3,4,5,6];
+
+    $scope.doRefresh = function() {
+      console.log("AM refreshing");
+      $http.get('http://192.168.1.106:4000/candidates')
+        .success(function(newItems) {
+          $scope.items = newItems;
+          console.log("AM success");
+        })
+        .error(function(error){
+          alert("hey error!!")
+          console.log("AM error");
+        })
+        .finally(function() {
+          $scope.$broadcast('scroll.refreshComplete');
+        });
+    };
+ })
+
+  .controller('interviewDetailsController', function() {
 	});
