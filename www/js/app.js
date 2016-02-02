476dae89c1ad82afa0bfecfb52a53887d60a93be
dinesba <dineshudt17@gmail.com>
2016-02-02 14:06:03 +0530

    Dinesh: Moved the run configuration to seperate file and exclude that from run during test

--- www/js/app.js
+++ www/js/app.js
@@ -6,54 +6,7 @@
 
 angular.module('starter', ['ionic', 'ui.router', 'ngCordova', 'ngResource'])
 // TODO: Move this into a properties/json file that is read in when the app starts
-  .constant('apiUrl', '192.168.1.106:4000')
-
-.run(function ($cordovaSplashscreen, $rootScope, $ionicPlatform, recruitFactory) {
-  'use strict';
-
-  $ionicPlatform.ready(function () {
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-
-    if (window.StatusBar) {
-      window.StatusBar.styleDefault();
-    }
-
-    /* Get roles and Skills */
-    var request = 0;
-    recruitFactory.getSkills(function(skills) {
-      $rootScope.skills = skills;
-      request++;
-      hideSplashScreen();
-    });
-
-    recruitFactory.getRoles(function(roles) {
-      $rootScope.roles = roles;
-      request++;
-      hideSplashScreen();
-    });
-
-    recruitFactory.getInterviewTypes(function(interview_types) {
-      $rootScope.interview_types = interview_types;
-      request++;
-      hideSplashScreen();
-    });
-
-    function hideSplashScreen() {
-      if(request >= 3) {
-        // $cordovaSplashscreen.hide();     // TODO: Fix later
-      }
-    }
-  });
-})
+.constant('apiUrl', '192.168.1.106:4000')
 
 .config(function ($stateProvider, $urlRouterProvider) {
   'use strict';
---
+++ www/js/run.js
@@ -0,0 +1,50 @@
+angular.module('starter')
+
+.run(function ($cordovaSplashscreen, $rootScope, $ionicPlatform, recruitFactory) {
+  'use strict';
+
+  $ionicPlatform.ready(function () {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+
+    if (window.StatusBar) {
+      window.StatusBar.styleDefault();
+    }
+
+    /* Get roles and Skills */
+    var request = 0;
+  //  if (envService.is('test')) {
+      recruitFactory.getSkills(function(skills) {
+        $rootScope.skills = skills;
+        request++;
+        hideSplashScreen();
+      });
+
+      recruitFactory.getRoles(function(roles) {
+        $rootScope.roles = roles;
+        request++;
+        hideSplashScreen();
+      });
+
+      recruitFactory.getInterviewTypes(function(interview_types) {
+        $rootScope.interview_types = interview_types;
+        request++;
+        hideSplashScreen();
+      });
+    //}
+
+    function hideSplashScreen() {
+      if(request >= 3) {
+        // $cordovaSplashscreen.hide();     // TODO: Fix later
+      }
+    }
+  });
+});
