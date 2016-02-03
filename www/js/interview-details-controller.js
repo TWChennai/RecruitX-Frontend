87b1877206ac3d41d738ea52b7c60ea75f2adfd7
dinesba <dineshudt17@gmail.com>
2016-02-03 17:39:58 +0530

    Dinesh: Show the role which was not shown previously in interview_details_page

--- www/js/interview-details-controller.js
+++ www/js/interview-details-controller.js
@@ -1,11 +1,12 @@
 angular.module('recruitX')
-  .controller('interviewDetailsController', ['$scope', '$stateParams', 'recruitFactory', function ($scope, $stateParams, recruitFactory) {
+  .controller('interviewDetailsController', ['$scope', '$stateParams', 'recruitFactory', '$rootScope', function ($scope, $stateParams, recruitFactory, $rootScope) {
     'use strict';
 
     $scope.interview = {};
 
     recruitFactory.getInterview($stateParams.id, function(interview) {
       $scope.interview = interview;
+      $scope.interview.candidate.role = $rootScope.roles[interview.candidate.role_id];
     });
 
     $scope.endTime = function(startTime) {
