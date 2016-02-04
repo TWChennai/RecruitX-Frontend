abc5912c66a2d0d638c6ef81ae047f64cf2ef0dd
Vijay Raghavan Aravamudhan <avijayr - at - gmail - dot - com>
2016-02-04 08:25:42 +0530

    Vijay: Use the same version of 'angular-mocks' as that of 'angular'.

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
 
 npm install
+bower install
 
 karma start tests/unit-tests.conf.js
 
-# echo "TODO: Need to fix errors reported by eslint and then remove this dummy echo line!!!"
-
 eslint --fix .
+
+# Note: Do not move this echo above the previous line - since then the exit code is non-zero (till the eslint errors are fixed)
+echo "TODO: Need to fix errors reported by eslint and then remove this dummy echo line!!!"
