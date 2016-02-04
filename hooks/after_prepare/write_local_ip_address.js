4e246fe7d219588b74a3ef000dfecaf5d738d6ea
Vijay Raghavan Aravamudhan <avijayr - at - gmail - dot - com>
2016-02-04 08:20:10 +0530

    Vijay: Added hook to capture ip address into a file after platform is prepared.

---
+++ hooks/after_prepare/write_local_ip_address.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+var isRelease = /--release/.test(process.env.CORDOVA_CMDLINE),
+  path = require('path'),
+  glob = require('glob'),
+  console = require('console'),
+
+  runShell = function (command) {
+    var exec = require('child_process').exec;
+
+    function puts(error, stdout, stderr) {
+      console.log(stdout);
+    }
+
+    exec(command, puts);
+  },
+
+  rootDir = process.argv[2],
+  platformsPath = path.join(rootDir, 'platforms');
+
+glob(path.join(platformsPath, '**', 'index.html'), function (er, files) {
+  var outputFile = path.join(path.dirname(files[0]), 'ip_address.txt');
+  if (isRelease === true) {
+    // TODO: Public domain name or IP address for production needs to be inserted here
+    runShell('echo \'http://128.199.136.71\' > ' + outputFile);
+  } else {
+    // TODO: This shell command is specific to Mac OSX - might not work on other OSes
+    runShell('ifconfig | grep -v \'127.0.0.1\' | egrep -o \'inet ([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})\' | tail -n 1 | awk -F \'[ ]\' \'{print "http://"$2":4000"}\' > ' + outputFile + ' && [ -s ' + outputFile + ' ] || echo \'http://10.0.2.2:4000\' > ' + outputFile);
+  }
+});
