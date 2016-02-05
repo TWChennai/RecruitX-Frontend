eb8e15fd9aba743d809bf94414cb7f79462a06fd
Maha Lakshme <mahalaks@thoughtworks.com>
2016-02-05 11:52:21 +0530

    #29:Maha:Added the functionality such that the user cannot go back once user is loggedin

--- www/js/login-controller.js
+++ www/js/login-controller.js
@@ -1,11 +1,14 @@
 angular.module('recruitX')
 
-.controller('loginController', function($scope, $state, oktaSigninWidget, loggedinUserStore) {
+.controller('loginController', function($scope, $state, $ionicHistory, oktaSigninWidget, loggedinUserStore) {
     oktaSigninWidget.renderEl(
       { el: '#okta-login-container' },
       function (res) {
         if (res.status === 'SUCCESS') {
             loggedinUserStore.storeUser(res.user);
+            $ionicHistory.nextViewOptions({
+              disableBack: true
+            });
             $state.go('panelist-signup');
         }
       }
