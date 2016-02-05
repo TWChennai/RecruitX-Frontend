550ec345e09a04f4fc5f1af30994b7509b94b25a
Maha Lakshme <mahalaks@thoughtworks.com>
2016-02-05 12:52:04 +0530

    Maha:Fixed the skill helper service that was accidentally changed

--- www/js/helper-services.js
+++ www/js/helper-services.js
@@ -1,19 +1,25 @@
 angular.module('recruitX')
-  .service('skillHelperService', function () {
-    this.formatAllSkills = function (skills, other_skills) {
-      var all_skills = [];
+  .service('skillHelperService', function ($filter, MasterData) {
+       'use strict';
+       this.formatAllSkills = function (candidate_skills, other_skills) {
+       var all_skills = [];
+       var skills = MasterData.getSkills();
 
-      // TODO: Please use a consistent for construct
-      angular.forEach(skills, function (skill) {
-        if (skill.name !== 'Other') {
-          all_skills.push(skill.name);
-        }
-      });
-
-      all_skills.push(other_skills);
-      return all_skills.join(', ');
-    };
-  })
+       // TODO: Please use a consistent for construct
+       angular.forEach(candidate_skills, function (skill) {
+         // TODO: Hard-coding a magic number doesn't convey the meaning. Move this to app.js where the skills are loaded for the first time.
+         var other_skill_id = 5;
+         if (skill.id !== other_skill_id) {
+           var role = ($filter('filter')(skills, {
+             id: skill.id
+           }))[0];
+           all_skills.push(role.name);
+         }
+       });
+       all_skills.push(other_skills);
+       return all_skills.join(', ');
+     };
+   })
   .service('oktaSigninWidget', function(endpoints){
       return new OktaSignIn({baseUrl: endpoints.oktaUrl})
   })
