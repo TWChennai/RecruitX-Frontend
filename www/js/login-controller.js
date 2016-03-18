f07a30505034eee4a78253f609c4626d917974f6
Subha <subhamanoharanbe@gmail.com>
2016-03-18 11:58:17 +0530

    Subha/Sukanya: Getting experience from UI for test users

--- www/js/login-controller.js
+++ www/js/login-controller.js
@@ -1,25 +1,58 @@
 angular.module('recruitX')
-  .controller('loginController', ['$rootScope', '$scope', '$state', '$ionicHistory', 'oktaSigninWidget', 'loggedinUserStore', 'recruitFactory', function ($rootScope, $scope, $state, $ionicHistory, oktaSigninWidget, loggedinUserStore, recruitFactory) {
+  .controller('loginController', ['$ionicPopup', '$rootScope', '$scope', '$state', '$ionicHistory', 'oktaSigninWidget', 'loggedinUserStore', 'recruitFactory', function ($ionicPopup, $rootScope, $scope, $state, $ionicHistory, oktaSigninWidget, loggedinUserStore, recruitFactory) {
     'use strict';
+
+    $scope.data = {};
 
     oktaSigninWidget.renderEl({
       el: '#okta-login-container'
     },
     function (res) {
       if (res.status === 'SUCCESS') {
-        recruitFactory.isRecruiter(res.user.profile.login.split('@')[0], function (response) {
-          loggedinUserStore.storeUser(res.user, response);
+        var loginName = res.user.profile.login.split('@')[0];
+        if(loginName === "ppanelist" || loginName === "ppanelistp"){
+          var myPopup = $ionicPopup.show({
+            template: '<input ng-model="data.experience">',
+            title: 'Enter experience',
+            scope: $scope,
+            buttons: [
+              {
 
-          $ionicHistory.nextViewOptions({
-            disableBack: true,
-            historyRoot: true
+                text: '<b>Done</b>',
+                onTap: function(e) {
+                  return $scope.data.experience;
+                }
+              }
+            ]
           });
 
-          $ionicHistory.clearCache().then(function(){
-            $state.go('tabs.interviews');
-            $rootScope.$broadcast('load:masterData');
+          myPopup.then(function(experience_input) {
+            recruitFactory.isRecruiter(experience_input + loginName, function (response) {
+              loggedinUserStore.storeUser(res.user, response);
+              $ionicHistory.nextViewOptions({
+                disableBack: true,
+                historyRoot: true
+              });
+              $ionicHistory.clearCache().then(function(){
+                $state.go('tabs.interviews');
+                $rootScope.$broadcast('load:masterData');
+              });
+            });
           });
-        });
+         }
+        else {
+          recruitFactory.isRecruiter(loginName, function (response) {
+            loggedinUserStore.storeUser(res.user, response);
+            $ionicHistory.nextViewOptions({
+              disableBack: true,
+              historyRoot: true
+            });
+            $ionicHistory.clearCache().then(function(){
+              $state.go('tabs.interviews');
+              $rootScope.$broadcast('load:masterData');
+            });
+          });
+        }
       }
     });
   }]);
