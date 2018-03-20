f3758cc98cca0a1bf7cf8d19950b8f3382ac5bb9
Aravind P L <aravindp@thoughtworks.com>
2018-03-20 13:03:07 +0530

    #16 [AravindPl/Jeremy] Removed the panelist experience dialog box used for testing purpose.

--- www/js/login-controller.js
+++ www/js/login-controller.js
@@ -26,47 +26,17 @@ angular.module('recruitX')
     var loginCallBack = function (res) {
       if (res.status === 'SUCCESS') {
         var loginName = res.user.profile.login.split('@')[0];
-        if (loginName === 'ppanelist' || loginName === 'ppanelistp') {
-          var myPopup = $ionicPopup.show({
-            template: '<input ng-model="data.experience">',
-            title: 'Enter experience',
-            scope: $scope,
-            buttons: [
-              {
-                text: '<b>Done</b>',
-                onTap: function () {
-                  return $scope.data.experience;
-                }
-              }
-            ]
+        recruitFactory.isRecruiter(loginName, function (response) {
+          loggedinUserStore.storeUser(res.user, response);
+          $ionicHistory.nextViewOptions({
+            disableBack: true,
+            historyRoot: true
           });
-
-          myPopup.then(function (experience_input) {
-            recruitFactory.isRecruiter(experience_input + loginName, function (response) {
-              loggedinUserStore.storeUser(res.user, response);
-              $ionicHistory.nextViewOptions({
-                disableBack: true,
-                historyRoot: true
-              });
-              $ionicHistory.clearCache().then(function () {
-                $state.go('tabs.interviews');
-                $rootScope.$broadcast('load:masterData');
-              });
-            });
+          $ionicHistory.clearCache().then(function () {
+            $state.go('tabs.interviews');
+            $rootScope.$broadcast('load:masterData');
           });
-        } else {
-          recruitFactory.isRecruiter(loginName, function (response) {
-            loggedinUserStore.storeUser(res.user, response);
-            $ionicHistory.nextViewOptions({
-              disableBack: true,
-              historyRoot: true
-            });
-            $ionicHistory.clearCache().then(function () {
-              $state.go('tabs.interviews');
-              $rootScope.$broadcast('load:masterData');
-            });
-          });
-        }
+        });
       }
     };
 
